2ea85363db07f88da13c28f73e7403e59513ecbd
Fernando Paniagua <fernando.paniagua.python@gmail.com>
2025-10-16 11:52:59 +0200

    Create 33_ejercicio_ordenacion.py

---
+++ ejercicios/33_ejercicio_ordenacion.py
@@ -0,0 +1,13 @@
+'''
+Crear una clase Automóvil
+- Marca
+- Modelo
+- Número de plazas
+- Precio
+
+Crear 3 instancias de Automóvil y las guardamos en una lista
+
+Ordenación por número de plazas (ascendente)
+Ordenación por el precio (descendente)
+'''
+
