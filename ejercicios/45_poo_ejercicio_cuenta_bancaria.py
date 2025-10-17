d42ec3c6e870ed474565a37ac0838b91137f4b60
fpaniaguajava <fernando.paniagua.python@gmail.com>
2025-10-17 13:10:17 +0200

    Update 45_poo_ejercicio_cuenta_bancaria.py

--- ejercicios/45_poo_ejercicio_cuenta_bancaria.py
+++ ejercicios/45_poo_ejercicio_cuenta_bancaria.py
@@ -12,3 +12,60 @@ Métodos:
 
 Crear una cuenta, hacer operaciones con ella y mostrar los atributos de la cuenta por pantalla (__str__ y __repr__)
 '''
+from enum import Enum
+
+class Estado(Enum):
+    ACTIVA = 1
+    SUSPENDIDA = 2
+    DESCUBIERTA = 3
+
+class SuspendidaException(Exception):
+    def __init__(self, *args):
+        super().__init__(*args) # Invocación al constructor de la clase padre
+
+class Cuenta:
+    def __init__(self, cliente : str, numero : str, estado : Estado, saldo : int):
+        self.cliente = cliente
+        self.numero = numero
+        self.estado = estado
+        self.saldo = saldo
+
+    def incrementar_importe(self, incremento : int):
+        print(f'Intentando incrementar el saldo en {incremento}')
+        assert(isinstance(incremento, int))
+        assert(incremento>0)
+        if (self.estado==Estado.SUSPENDIDA):
+            raise SuspendidaException('La cuenta está suspendida')
+        saldo_anterior = self.saldo
+        self.saldo+=incremento
+        if (saldo_anterior<0 and self.saldo>=0):
+            self.estado=Estado.ACTIVA
+
+    def retirar_importe(self, decremento : int):
+        print(f'Intentando decrementar el saldo en {decremento}')
+        assert(isinstance(decremento, int))
+        assert(decremento<0)
+        if (self.estado==Estado.SUSPENDIDA):
+            raise SuspendidaException('La cuenta está suspendida')
+        saldo_anterior = self.saldo
+        self.saldo+=decremento
+        if (saldo_anterior>=0 and self.saldo<0):
+            self.estado=Estado.DESCUBIERTA
+
+    def __str__(self):
+        return f'Cliente:{self.cliente}. Cuenta:{self.numero}. Estado:{self.estado}. Saldo:{self.saldo}'
+
+    def __repr__(self):
+        return self.__str__()
+  
+
+cuenta = Cuenta('Fernando','1234567890', Estado.ACTIVA, 10_000)
+print(cuenta)
+cuenta.retirar_importe(-5000)
+print(cuenta)
+cuenta.retirar_importe(-7000)
+print(cuenta)
+cuenta.incrementar_importe(2100)
+print(cuenta)
+cuenta.estado = Estado.SUSPENDIDA
+cuenta.incrementar_importe(900)
