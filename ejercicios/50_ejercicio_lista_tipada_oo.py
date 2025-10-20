4e4f952d9de3617a2098d13283acfa2926107925
Fernando Paniagua <fernando.paniagua.python@gmail.com>
2025-10-20 10:17:49 +0200

    Update 50_ejercicio_lista_tipada_oo.py

---
+++ ejercicios/50_ejercicio_lista_tipada_oo.py
@@ -0,0 +1,3 @@
+# Definir una clase Pelicula con los atributos Título y año.
+# Construir una estructura de datos de tipo <list> que sólo admita objetos Pelicula
+# (Ejercicio 51) Construir una estructura de datos de tipo <dict> que sólo admita objetos Pelicula
